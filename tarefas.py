0fddd5e3da1fee85be328e2a510524fbdfc5f23f
Erik <erik100.eliasv@hotmail.com>
2025-01-30 10:19:17 -0300

    Merge do codigo

--- tarefas.py
+++ tarefas.py
@@ -1,5 +1,6 @@
-import customtkinter as ct
 import tkinter as tk
+import customtkinter as ct
+import customtkinter as ctk
 
 nova_janela = None
 
@@ -8,7 +9,7 @@ def janela():
 
     if nova_janela and nova_janela.winfo_exists():
         nova_janela.destroy()
-        
+
     nova_janela = ct.CTkToplevel(root)
     nova_janela.title("Adicionar Tarefa")
     nova_janela.geometry("300x200")
@@ -24,33 +25,53 @@ def janela():
     botao2 = ct.CTkButton(master=nova_janela, text="Adicionar", font=("Arial", 14), corner_radius=10, bg_color="white", text_color="white", command=lambda: botao_adicionar(entrada.get(), lista))
     botao2.place(x=80, y=80)
 
-def concluido(envent):
-    index = envent.widget.curselection()
+def concluido(event):
+    index = event.widget.curselection()
     if index:
-        item_selecionado = envent.widget.get(index)
+        item_selecionado = event.widget.get(index)
         lista.delete(index)
         lista3.insert("end", item_selecionado)
 
 
-def botao_remover():
-    def remover_item():
+def exibir_remover(event2):
+    menu.post(event2.x_root, event2.y_root)
+def remover_item():
+    index = lista.curselection()
+    index2 = lista3.curselection()
+    if index:
+        lista.delete(index)
+            
+    elif index2:
+        lista3.delete(index2) 
+
+def editar_item():
+    outra_janela = ct.CTkToplevel(root)
+    outra_janela.geometry("300x200")
+    label3 = ct.CTkLabel(master=outra_janela, text="Edite a sua tarefa:", font=("Arial", 14, "bold"))
+    label3.grid(pady=3, padx=5)
+    entrada = ct.CTkEntry(master=outra_janela, width=250)
+    entrada.place(x=5, y=35)
+    def confirmar():
+        trocar = entrada.get()
         index = lista.curselection()
-        index2 = lista3.curselection()
         if index:
             lista.delete(index)
-            outro_botão.destroy()
-        elif index2:
-            lista3.delete(index2)
-            outro_botão.destroy()
-    outro_botão = ct.CTkButton(root, text="Remover", font=("Arial", 14, "bold"), width=15, command=remover_item, bg_color="#FDB678", fg_color="#FF7314")
-    outro_botão.place(y=408, x=300)
+            lista.insert("end", trocar)       
+        outra_janela.destroy()
+    botao_adicionar = ct.CTkButton(master=outra_janela, text="Adicionar", fg_color="blue", command=confirmar)
+    botao_adicionar.place(x=80, y=80)
+
 
 root = ct.CTk()
 root.title("Gerenciador de tarefas")
 root.geometry("390x518")
 root.config(bg="#FDB678")
-
 root.resizable(False,  False)
+
+menu = tk.Menu(root, tearoff=0)
+menu.add_command(label = "Remover", command=remover_item)
+menu.add_command(label="Editar", command=editar_item)
+root.bind("<Button-3>", exibir_remover)
 
 label = ct.CTkLabel(master= root, text= "Olá, bem vindo ao seu", font=("Inter", 20), bg_color="#FDB678")
 label.grid(sticky="W", padx=10, pady=40)
@@ -73,17 +94,14 @@ linha3.place(y=150, x=200)
 linha = ct.CTkFrame(master=root, width=495, height=2.5)
 linha.place(y=400)
 
-lista = tk.Listbox(root, width=41, height=20, bg="#FDB678", fg="black")
-lista.place(x=0, y=179)
+lista = tk.Listbox(root, width=39, height=20, bg="#FDB678", fg="black")
+lista.place(x=0, y=180)
 lista.bind("<Double-1>", concluido)
 
 lista3 = tk.Listbox(root, width=39, height=20,bg="#FDB678", fg="black")
-lista3.place(x=252, y=179)
+lista3.place(x=250, y=180)
 
 botao = ct.CTkButton(master=root, text="Adicionar Tarefa", font=("Arial", 14),corner_radius=10 ,bg_color="#FDB678", fg_color="#FF7314", text_color="White", height=35, command=janela)
 botao.place(x=130, y=450)
-
-botao_editar = ct.CTkButton(master=root, text="Editar", font=("Arial", 14, "bold"), width=15, fg_color="#FF7314",bg_color="#FDB678", text_color="white", command=botao_remover)
-botao_editar.place(x=3, y=408)
 
 root.mainloop()
